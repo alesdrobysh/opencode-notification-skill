107df54d3ba4427e496ae58e1dd1693197fb0aac
Ales Drobysh <ales.drobysh@codility.com>
2026-03-11 14:46:33 +0100

    Fix notifications broken on macOS 26: drop NSUserNotification (removed in 26), use display notification

--- notify.sh
+++ notify.sh
@@ -60,29 +60,7 @@ play_sound() {
 show_alert() {
   local title="$1"
   local message="$2"
-  local icon="$SKILL_DIR/logo.png"
-  osascript - "$title" "$message" "$icon" &>/dev/null <<'APPLESCRIPT' &
-use framework "Foundation"
-use framework "AppKit"
-use scripting additions
-
-on run argv
-  set theTitle to item 1 of argv
-  set theMessage to item 2 of argv
-  set iconPath to item 3 of argv
-
-  set notif to current application's NSUserNotification's alloc()'s init()
-  notif's setTitle_(theTitle)
-  notif's setInformativeText_(theMessage)
-
-  if (current application's NSFileManager's defaultManager()'s fileExistsAtPath_(iconPath)) then
-    set theImage to current application's NSImage's alloc()'s initWithContentsOfFile_(iconPath)
-    notif's setContentImage_(theImage)
-  end if
-
-  current application's NSUserNotificationCenter's defaultUserNotificationCenter()'s deliverNotification_(notif)
-end run
-APPLESCRIPT
+  osascript -e "display notification \"$message\" with title \"$title\"" &>/dev/null &
 }
 
 get_sound_file() {
